2a40d9e3f6931d7c214843ac11c23b1bb5a6a705
Yash B Joshi <yashbjoshi.99@gmail.com>
2022-10-07 19:51:40 +0530

    7

--- Javascript/welcomeUser.js
+++
@@ -1,7 +0,0 @@
-var readlineSync = require('readline-sync');
-
-var userName = readlineSync.question('May I hv your name ? ');
-
-var welcomeMessage = "Welcome! " + userName;
-
-console.log(welcomeMessage);
